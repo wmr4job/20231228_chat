8597e6bde919a5ace888d265a7998da34e0801bc
Milly <wmr4job@gmail.com>
2023-12-29 10:47:20 +0800

    Update

--- 20231228_chat.py
+++ 20231228_chat.py
@@ -5,44 +5,41 @@ import os
 # 讀取檔案
 def read_file(filename):
 	text = []
-	with open(filename, 'r', encoding='utf-8') as f:
+	with open(filename, 'r', encoding='utf-8-sig') as f: # -sig去除讀檔產生的編碼紀錄文字
 		for line in f:
 			text.append(line.strip('\n'))
-	print(text)
 	return text
 
-#判斷人名，調整內容
+#判斷人名，格式轉換
 def whosays(text):
-	chat = []
+	new_chat = [] # 儲存轉換後內容
+	who = None # 先宣告變數為"無"，避免第一行無人名時，程式執行發生錯誤
 	for d in text:
 		if 'Allen' in d:
 			who = 'Allen: '
 		elif 'Tom' in d:
 			who = 'Tom: '
-		else:
-			chat.append([who, d])
-	# print(chat)
-	return chat
+		elif who: # 如果who有值才執行
+			new_chat.append(who + d)
+	return new_chat
 
 # 寫入檔案
-def write_file(filename, chat):
+def write_file(filename, new_chat):
 	with open(filename, 'w', encoding='utf-8') as f:
-		for d in range(len(chat)):
-			for i in range(2):
-				f.write(chat[d][i])
-			f.write('\n')
+		for d in new_chat:
+			f.write(d + '\n')
 
+# 主程式
+def main():
+	filename = 'input.txt'
+	if os.path.isfile(filename): # 檢查檔案是否存在
+		text = read_file(filename) # 紀錄讀取檔案後的內容
+		new_chat = whosays(text) # 紀錄格式轉換後的內容
+		write_file('output.txt', new_chat) # 寫入的檔案名稱、內容
+		print('轉換完成！')
+	else:
+		print('檔案不存在')
 
-# 檢查檔案是否存在
-filename = 'input.txt'
-if os.path.isfile(filename):
-	text = read_file(filename)
-	chat = whosays(text)
-	print(chat)
-	write_file('output.txt', chat)
-else:
-	print('檔案不存在')
-
-
+main()
 
 
